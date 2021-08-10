8564937fc2c371916c35b769322fa5a18907b32a
8cw <54337835+8cw@users.noreply.github.com>
2021-08-11 11:56:13 +1200

    Placement improvements (#23)
    * tower placement sound effect
    
    * Add cancel button
    
    * Placement Circle
    
    * Make illegal hover
    
    * Right click cancel placement

---
+++ students-vs-teachers/SoundPlayer.cs
@@ -0,0 +1,20 @@
+﻿// <summary>
+// Handles game sound effects.
+// </summary>
+// <copyright file="SoundPlayer.cs" company="HBHS">
+// Copyright (c) HBHS. All rights reserved.
+// </copyright>
+
+namespace Students_vs_teachers
+{
+    /// <summary>
+    /// Handles game sounds.
+    /// </summary>
+    internal class SoundPlayer
+    {
+        /// <summary>
+        /// A "swoosh" sound effect for when tower item is placed.
+        /// </summary>
+        public static readonly System.Media.SoundPlayer TowerPlacement = new System.Media.SoundPlayer(Properties.Resources.tower_placement);
+    }
+}
